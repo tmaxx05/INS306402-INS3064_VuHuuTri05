bb1d38bf5b18d82101785970f8e61663a80e0ac5
tmaxx05 <trimaxvu@gmail.com>
2026-04-21 12:04:13 +0700

    Delete Ses_10/config directory

--- Ses_10/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/database.php
-return[
-    'host' => 'localhost',
-    'dbname' => 'shop_db', // Ensure you create this database and a 'products' table
-    'user' => 'root',
-    'pass' => ''
-];
